2164402c88909c9e15ce44e973911451574a8f41
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-11-21 13:23:22 +0530

    first commit

---
+++ .next/dev/server/middleware-build-manifest.js
@@ -0,0 +1,26 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/47d5f_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0b4d1ca4._.js",
+    "static/chunks/47d5f_next_dist_compiled_react-dom_a0819330._.js",
+    "static/chunks/47d5f_next_dist_compiled_react-server-dom-turbopack_16879940._.js",
+    "static/chunks/47d5f_next_dist_compiled_next-devtools_index_d19190d2.js",
+    "static/chunks/47d5f_next_dist_compiled_d2246d60._.js",
+    "static/chunks/47d5f_next_dist_client_8c8105aa._.js",
+    "static/chunks/47d5f_next_dist_8c86452f._.js",
+    "static/chunks/47d5f_@swc_helpers_cjs_f7339b79._.js",
+    "static/chunks/Documents_Data_blogfly_a0ff3932._.js",
+    "static/chunks/turbopack-Documents_Data_blogfly_946943c7._.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gWa9sWop4ypI/fbzAQ1lFmfgalYHaadUfcSpnX3wAvU=\"\n}"
